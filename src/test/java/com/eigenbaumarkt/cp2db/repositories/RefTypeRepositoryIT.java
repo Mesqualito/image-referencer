1b3df571217b4bd5a1a535d5e70bfcabf4871bc8
Mesqualito <jochen@gebsattel.rocks>
2020-06-14 16:34:23 +0200

    Load data and test RefTypeRepository (3)

--- src/test/java/com/eigenbaumarkt/cp2db/repositories/RefTypeRepositoryIT.java
+++ src/test/java/com/eigenbaumarkt/cp2db/repositories/RefTypeRepositoryIT.java
@@ -1,34 +1,26 @@
 package com.eigenbaumarkt.cp2db.repositories;
 
 import com.eigenbaumarkt.cp2db.domain.RefType;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class) // will load Spring Context
+@SpringBootTest
 public class RefTypeRepositoryIT {
 
-
-    @Autowired // will autowire an object of UnitOfMeasureRepository
-            RefTypeRepository refTypeRepository;
-
-    @Before
-    public void setUp() throws Exception {
-    }
+    @Autowired
+    RefTypeRepository refTypeRepository;
 
     @Test
     @DirtiesContext // will reload Spring Context
     public void findByTypeNameGtin() {
 
         Optional<RefType> refTypeOptional = refTypeRepository.findByTypeName("GTIN");
-
         assertEquals("GTIN", refTypeOptional.get().getTypeName());
     }
 
@@ -36,7 +28,6 @@ public class RefTypeRepositoryIT {
     public void findByTypeNameIptc() {
 
         Optional<RefType> refTypeOptional = refTypeRepository.findByTypeName("IPTC");
-
         assertEquals("IPTC", refTypeOptional.get().getTypeName());
     }
 }
